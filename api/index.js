fac61cd8c1b180382a6f7c8fd162e633bb3eabba
ItsIvanPsk <ivanfigueredo.et@gmail.com>
2025-09-11 08:12:20 +0200

    Vercel Fix

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
-const server = require('../dist/ssr/index.js')
-
-module.exports = server
+try {
+  const quasarServer = require('../dist/ssr/index.js')
+  module.exports = quasarServer.default || quasarServer
+} catch (err) {
+  console.error('SSR server not found, did you run `quasar build -m ssr`?', err)
+  throw err
+}
