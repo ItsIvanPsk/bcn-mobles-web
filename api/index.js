22431d028796d7c2f7d02aecc9bb5cabb28b393e
ItsIvanPsk <ivanfigueredo.et@gmail.com>
2025-09-11 10:26:09 +0200

    Fix

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
-import { handler } from '../dist/ssr/index.js'
+import * as quasarServer from '../dist/ssr/index.js'
 
-export default function (req, res) {
-  return handler(req, res)
+export default async function handler(req, res) {
+  try {
+    const html = await quasarServer.render({ req, res })
+    res.setHeader('Content-Type', 'text/html')
+    res.end(html)
+  } catch (err) {
+    console.error('[SSR Error]', err)
+    res.statusCode = 500
+    res.end('Internal Server Error')
+  }
 }
